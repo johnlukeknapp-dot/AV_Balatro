6c4b9227fd849f94220499f5b3b88f1a569f91e7
johnlukeknapp-dot <johnlukeknapp@gmail.com>
2026-05-31 22:29:51 -0400

    Added Propagule

---
+++ localization/en-us.lua
@@ -0,0 +1,19 @@
+return{
+    descriptions = {
+        Joker = {
+            j_astravol_joker1 = {
+                name = 'Joker1',
+                text = {
+                    '{C:chips}+#1#{} chips'
+                }
+            },
+            j_astravol_Propagule = {
+                name = 'Propagule',
+                text = {
+                    "{C:green}#1# in #2#{} chance to",
+                    "Retrigger each played {C:attention}5{}, or {C:attention}2{}",
+                }
+            }
+        }
+    }
+}
--- src/jokers/joker1.lua
+++ src/jokers/joker1.lua
@@ -27,3 +27,27 @@ SMODS.Joker{
             end
     end
 }
+
+--JMC
+SMODS.Joker{
+    key = 'Propagule',
+    atlas = 'placeholders',
+    pos = { x = 1, y = 0 },
+    rarity = 1,
+    cost = 4,
+    config = {extra = { repetitions = 1, odds = 2} },
+    loc_vars = function(self, info_queue, card)
+        local numerator, denominator = SMODS.get_probability_vars(card, 1, card.ability.extra.odds, 'astravol_Propagule')
+        return { vars = { numerator, denominator } }
+     end,
+    calculate = function(self, card, context)
+        if context.repetition and context.cardarea == G.play and SMODS.pseudorandom_probability(card, 'astravol_Propagule', 1, card.ability.extra.odds) then
+            local id = context.other_card:get_id()
+            if id == 2 or id == 5 then
+                return {
+                    repetitions = card.ability.extra.repetitions
+                }
+            end
+        end
+    end
+}
